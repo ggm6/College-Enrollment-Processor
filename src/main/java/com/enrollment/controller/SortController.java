9c9e5cf62d90f8d2b6f466722c4b41b7773a3dfa
Garrett McDonnell <ggm6@zips.uakron.edu>
2026-07-10 02:59:56 -0400

    Optimized sorting logic to filter schedules with duplicate class names and time overlaps upfront, removed unecessary tests

--- src/main/java/com/enrollment/controller/SortController.java
+++ src/main/java/com/enrollment/controller/SortController.java
@@ -1,15 +1,15 @@
 package com.enrollment.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.stream.IntStream;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.enrollment.request.Course;
@@ -19,12 +19,10 @@ import com.enrollment.request.Schedule;
 public class SortController {
 
 	@PostMapping(value = "/sort")
-	public ArrayList<Schedule> processScheduleSortingRequest(@RequestBody List<Course> courses) {
-		ArrayList<Schedule> allPossibleSchedules = getAllSchedulePermutations(courses);
-		removeSchedulesWithDuplicateCourseNames(allPossibleSchedules);
-		removeSchedulesWithTimeConflicts(allPossibleSchedules);
-		orderSchedules(allPossibleSchedules);
-		return allPossibleSchedules;
+	public List<Schedule> processScheduleSortingRequest(@RequestBody List<Course> courses,
+			@RequestParam(name="onlyReturnSchedulesWithMostPossibleClasses", defaultValue = "true") Boolean onlyReturnSchedulesWithMostPossibleClasses) {
+		Set<Schedule> allPossibleSchedules = getAllSchedulePermutations(courses, onlyReturnSchedulesWithMostPossibleClasses);
+		return orderSchedules(allPossibleSchedules);
 	}
 	
 	/*
@@ -32,14 +30,19 @@ public class SortController {
 	 * https://stackoverflow.com/questions/29910312/algorithm-to-get-all-the-
 	 * combinations-of-size-n-from-an-array-java by answerer: Alex Salauyou
 	 */
-	public ArrayList<Schedule> getAllSchedulePermutations(final List<Course> courses) {
+	public Set<Schedule> getAllSchedulePermutations(final List<Course> courses, Boolean onlyReturnSchedulesWithMostPossibleClasses) {
 
-		ArrayList<Schedule> allPossibleSchedules = new ArrayList<Schedule>();
+		Set<Schedule> allPossibleSchedules = new HashSet<Schedule>();
 
 		// Accommodates variable number of classes in a schedule (if, say, 4 courses are chosen, schedule lengths may be anywhere from 1 to 4 classes long)
-		int[] scheduleLengths = IntStream.range(1, courses.size() + 1).toArray();
+		int numberOfDistinctClasses = courses.stream().map(Course::getName).collect(Collectors.toCollection(HashSet::new)).size();
 
-		for (int scheduleLength : scheduleLengths) {
+		for (int scheduleLength = numberOfDistinctClasses; scheduleLength >= 0; --scheduleLength) {
+			
+			// We'd usually like to stop generating schedules once we have every possible schedule containing the maximum number of distinct courses.
+			// Meaning, if 4 courses are chosen, and a ton of combinations of courseName + prof + time slots are available, we generally only want the schedules with a "choose 4" combination of courses, or the highest number we can do. 
+			if (Boolean.TRUE.equals(onlyReturnSchedulesWithMostPossibleClasses) && scheduleLength < numberOfDistinctClasses && !allPossibleSchedules.isEmpty())
+				break;
 			
 			int[] courseIndices = new int[scheduleLength];
 			
@@ -47,8 +50,7 @@ public class SortController {
 			if (scheduleLength == 1) {
 				for (int i = 0; (courseIndices[i] = i) < scheduleLength - 1; i++)
 					;
-				Schedule schedule = getScheduleByIndexSequence(courses, courseIndices);
-				allPossibleSchedules.add(schedule);
+				addScheduleByIndexSequence(allPossibleSchedules, courses, courseIndices);
 			}
 
 			for (;;) {
@@ -63,48 +65,37 @@ public class SortController {
 				for (++i; i < scheduleLength; i++) // fill up remaining items
 					courseIndices[i] = courseIndices[i - 1] + 1;
 
-				Schedule schedule = getScheduleByIndexSequence(courses, courseIndices);
-				if ( !allPossibleSchedules.contains(schedule) )
-					allPossibleSchedules.add(schedule);
+				addScheduleByIndexSequence(allPossibleSchedules, courses, courseIndices);
 			}
 		}
 
 		return allPossibleSchedules;
 	}
 
-	private Schedule getScheduleByIndexSequence(final List<Course> courses, int[] courseIndices) {
+	private void addScheduleByIndexSequence(Set<Schedule> allPossibleSchedules, final List<Course> courses, int[] courseIndices) {	
 		Schedule result = new Schedule();
-		for (int pos = 0; pos < courseIndices.length; ++pos) {
+		for (int pos = 0; pos < courseIndices.length; ++pos) {			
 			int index = courseIndices[pos];
 			Course course = courses.get(index);
+			
+			if ( scheduleHasDuplicateCourseNameOrTimeOverlap(result, course) )
+				return;
+			
 			result.addCourseAt(pos, course);
 		}
-		return result;
+		allPossibleSchedules.add(result);
+	}
+	
+	private boolean scheduleHasDuplicateCourseNameOrTimeOverlap(Schedule schedule, Course courseToAdd) {
+		return schedule.getCourses().stream().anyMatch(c -> c.getName().equals(courseToAdd.getName()) || c.overlaps(courseToAdd));
 	}
 
-	public void removeSchedulesWithDuplicateCourseNames(ArrayList<Schedule> allPossibleSchedules) {
-		Iterator<Schedule> iter = allPossibleSchedules.iterator();
-		while (iter.hasNext()) {
-			Schedule schedule = iter.next();
-			HashSet<String> distinctCourseNames = schedule.getDistinctCourseNames();
-			if ( schedule.getCourses().size() != distinctCourseNames.size() )
-				iter.remove();
-		}
-	}
-
-	public void removeSchedulesWithTimeConflicts(ArrayList<Schedule> allPossibleSchedules) {
-		ListIterator<Schedule> iter = allPossibleSchedules.listIterator();	
-			while (iter.hasNext()) {
-				Schedule schedule = iter.next();
-				if ( schedule.containsCourseOverlaps() )
-					iter.remove();
-		}
-	}
-
-	private void orderSchedules(ArrayList<Schedule> allPossibleSchedules) {
+	private List<Schedule> orderSchedules(Collection<Schedule> allPossibleSchedules) {
 		for (Schedule schedule : allPossibleSchedules)
 			schedule.orderByStartTimeAscending();
 		
-		Collections.sort(allPossibleSchedules);
+		return allPossibleSchedules.stream()
+				.sorted(Comparator.comparingInt((Schedule s) -> s.getCourses().size()).reversed())
+			    .collect(Collectors.toList());
 	}
 }
